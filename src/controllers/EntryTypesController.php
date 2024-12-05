228364719dd89ba2996797982ccf01b020069e2e
matthew <mattdj@honcho.agency>
2024-12-05 16:59:06 +0000

    Add entry types permission

--- src/controllers/EntryTypesController.php
+++ src/controllers/EntryTypesController.php
@@ -3,23 +3,24 @@
 namespace boost\multie\controllers;
 
 use boost\multie\helpers\EntryTypesVueAdminTableHelper;
-use boost\multie\helpers\SectionSiteSettingsVueAdminTableHelper;
 use boost\multie\services\EntryTypeService;
 use Craft;
 use craft\web\Controller;
 use boost\multie\Plugin;
 use craft\web\Request;
-use nystudio107\seomatic\models\jsonld\DDxElement;
 
 class EntryTypesController extends Controller
 {
     const PATH = Plugin::HANDLE . '/entry-types';
 
     const ACTION_UPDATE = self::PATH . '/update';
+
     public function actionIndex(string $type = null): \yii\web\Response
     {
         $tableData = EntryTypesVueAdminTableHelper::data(\Craft::$app->entries->getAllEntryTypes());
-        $actions = EntryTypesVueAdminTableHelper::actions();
+        $actions = Craft::$app->user->checkPermission(Plugin::PERMISSION_EDIT_ENTRY_TYPES) ?
+            EntryTypesVueAdminTableHelper::actions() :
+            false;
         $columns = EntryTypesVueAdminTableHelper::columns();
 
         return $this->renderTemplate(self::PATH . '/index.twig', [
@@ -32,9 +33,7 @@ class EntryTypesController extends Controller
 
     public function actionUpdate(): \yii\web\Response
     {
-
-//        TODO: Add permissions
-//        $this->requirePermission(Plugin::PERMISSION_EDIT_FIELDS);
+        $this->requirePermission(Plugin::PERMISSION_EDIT_ENTRY_TYPES);
 
         /** @var Request $request */
         $request = Craft::$app->request;
